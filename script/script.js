9018287df0a60075ba1eaffacdd4cf24d3f890bd
Sepehr Aghapour <dev.sepehr@outlook.com>
2022-09-03 00:31:07 +0430

    Updated with ES6 syntax

--- script/script.js
+++ script/script.js
@@ -7,9 +7,9 @@ let noteDescription = $.getElementById("note-description");
 // preload
 let preload = $.querySelector(".loader");
 
-function loading() {
+let loading = () => {
   preload.classList.add("hidden");
-}
+};
 window.addEventListener("load", loading);
 
 // Note Option Accordion
@@ -19,7 +19,7 @@ accordion.forEach((toolItemHeader) => {
     let toolItemBody = toolItemHeader.nextElementSibling;
 
     if (toolItemHeader.classList.contains("active")) {
-      toolItemBody.style.maxHeight = toolItemBody.scrollHeight + "px";
+      toolItemBody.style.maxHeight = `${toolItemBody.scrollHeight}px`;
     } else {
       toolItemBody.style.maxHeight = 0;
     }
@@ -28,9 +28,9 @@ accordion.forEach((toolItemHeader) => {
 
 // Note Demo getClass
 let notePreview = $.querySelector(".note-preview");
+let demoInputs = $.querySelectorAll(".demo-inputs");
 let h2Elem = $.querySelector(".note-preview-title");
 let notePreviewDescriptionText = $.querySelector(".note-preview-description-text");
-
 // Get input option Elements
 let titleColor = $.getElementById("title-color-set");
 let descriptionColor = $.getElementById("description-color-set");
@@ -38,34 +38,39 @@ let backgroundColor = $.getElementById("background-color-set");
 let titleSize = $.getElementById("title-size-set");
 let descriptionSize = $.getElementById("description-size-set");
 
+// all input on change event
+
 // titleColor OnChange Event
-function titleColorValue() {
-  h2Elem.style.color = titleColor.value;
-}
+let titleColorValue = () => titleColor.value;
 titleColor.addEventListener("input", titleColorValue);
 
 // DescriptionColor OnChange Event
-function descriptionColorValue() {
-  notePreviewDescriptionText.style.color = descriptionColor.value;
-}
+let descriptionColorValue = () => descriptionColor.value;
 descriptionColor.addEventListener("input", descriptionColorValue);
 
 // backgroundColor OnChange Event
-function backgroundColorValue() {
-  notePreview.style.backgroundColor = backgroundColor.value;
-}
+let backgroundColorValue = () => backgroundColor.value;
 backgroundColor.addEventListener("input", backgroundColorValue);
 
-function titleSizeValue() {
-  h2Elem.style.fontSize = titleSize.value + "px";
-}
+let titleSizeValue = () => `${titleSize.value}px`;
 titleSize.addEventListener("input", titleSizeValue);
 
-function descriptionSizeValue() {
-  notePreviewDescriptionText.style.fontSize = descriptionSize.value + "px";
-}
+let descriptionSizeValue = () => `${descriptionSize.value}px`;
 descriptionSize.addEventListener("input", descriptionSizeValue);
 
+let noteDemoHandler = () => {
+  h2Elem.style.color = titleColorValue();
+  notePreviewDescriptionText.style.color = descriptionColorValue();
+  notePreview.style.backgroundColor = backgroundColorValue();
+  h2Elem.style.fontSize = titleSizeValue();
+  notePreviewDescriptionText.style.fontSize = descriptionSizeValue();
+};
+
+demoInputs.forEach((inputs) => {
+  inputs.addEventListener("input", noteDemoHandler);
+});
+// set demo value on load
+window.addEventListener("load", noteDemoHandler);
 // add and remove button action
 let date;
 setInterval(() => {
@@ -77,7 +82,7 @@ let clearBtn = $.getElementById("clear");
 
 let notesLocalStorage = [];
 // save Button
-function addBtnAction() {
+let addBtnAction = () => {
   if (!noteTitleInput.value) {
     alert("عنوان نمیتواند خالی باشد");
   } else {
@@ -85,11 +90,11 @@ function addBtnAction() {
       id: notesLocalStorage.length + 1,
       title: noteTitleInput.value,
       description: noteDescription.value,
-      titleColor: titleColor.value,
-      descriptionColor: descriptionColor.value,
-      backgroundColor: backgroundColor.value,
-      titleSize: titleSize.value,
-      descriptionSize: descriptionSize.value,
+      titleColor: titleColorValue(),
+      descriptionColor: descriptionColorValue(),
+      backgroundColor: backgroundColorValue(),
+      titleSize: titleSizeValue(),
+      descriptionSize: descriptionSizeValue(),
       date: date,
     };
     notesLocalStorage.push(appendToStorage);
@@ -98,19 +103,19 @@ function addBtnAction() {
     clearBtnAction();
     noteTitleInput.focus();
   }
-}
+};
 addBtn.addEventListener("click", addBtnAction);
 
-function getLocalStorage(noteArray) {
+let getLocalStorage = (noteArray) => {
   localStorage.setItem("notes", JSON.stringify(noteArray));
-}
+};
 
 // add New Notes Function
 let noteAdded = $.querySelector(".notes-added");
-function notesGenerator(noteArray) {
+let notesGenerator = (noteArray) => {
   noteAdded.innerHTML = "";
   let border;
-  noteArray.forEach(function (note) {
+  noteArray.forEach((note) => {
     if (!note.description) {
       border = "unset";
     } else {
@@ -159,10 +164,10 @@ function notesGenerator(noteArray) {
         </div>`
     );
   });
-}
+};
 
 // get local storage on load
-function getDataOnLoad() {
+let getDataOnLoad = () => {
   let noteIndex = JSON.parse(localStorage.getItem("notes"));
 
   if (!noteIndex) {
@@ -172,18 +177,18 @@ function getDataOnLoad() {
   }
 
   notesGenerator(notesLocalStorage);
-}
+};
 window.addEventListener("load", getDataOnLoad);
 
 // Clear Button
-function clearBtnAction() {
+let clearBtnAction = () => {
   noteTitleInput.value = "";
   noteDescription.value = "";
-}
+};
 clearBtn.addEventListener("click", clearBtnAction);
 
 // click to delete note
-function deleteNote(noteid) {
+let deleteNote = (noteid) => {
   let noteIndex = JSON.parse(localStorage.getItem("notes"));
 
   notesLocalStorage = noteIndex;
@@ -195,14 +200,14 @@ function deleteNote(noteid) {
 
   getLocalStorage(notesLocalStorage);
   notesGenerator(notesLocalStorage);
-}
+};
 
 // dark mode
 let darkMode = $.querySelector(".night-mode");
 let modeIcon = $.getElementById("theme");
 let darkTheme = false;
 
-function darkModeBtn() {
+let darkModeBtn = () => {
   if (!darkTheme) {
     modeIcon.classList.remove("fa-sun");
     modeIcon.classList.add("fa-moon");
@@ -216,10 +221,10 @@ function darkModeBtn() {
     localStorage.setItem("theme", "dark");
     darkTheme = false;
   }
-}
+};
 darkMode.addEventListener("click", darkModeBtn);
 
-function windowOnLoadTheme() {
+let windowOnLoadTheme = () => {
   let currentTheme = localStorage.getItem("theme");
   if (currentTheme === null || currentTheme === "light") {
     darkTheme = false;
@@ -228,20 +233,19 @@ function windowOnLoadTheme() {
     darkTheme = true;
     darkModeBtn();
   }
-}
+};
 window.addEventListener("load", windowOnLoadTheme);
 
 // inputs keydown event
-noteTitleInput.addEventListener("keydown", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
+noteTitleInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
     noteDescription.focus();
   }
 });
 
-noteDescription.addEventListener("keydown", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
+noteDescription.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
     addBtnAction();
   }
 });
